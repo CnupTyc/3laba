080f00cef35d32e0dedbee4d88315044ea8bf979
CnupTyc <cnuptyc543@gmail.com>
2018-05-27 23:21:45 +0700

    Create menu

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -8,8 +8,24 @@ namespace Calculator
 {
     class Program
     {
+        static void Menu()
+        {
+            Console.WriteLine("\nНаучный калькулятор:\n");
+            Console.WriteLine("Выберите операцию:");
+            Console.WriteLine("\n1. Сложение");
+            Console.WriteLine("2. Вычитание");
+            Console.WriteLine("3. Умножение");
+            Console.WriteLine("4. Деление");
+            Console.WriteLine("5. Возведение в степень");
+            Console.WriteLine("6. Извлечение корня");
+            Console.WriteLine("7. Вычисление синуса");
+            Console.WriteLine("8. Вычисление косинуса");
+            Console.Write("9. Выход\n");
+        }
+
         static void Main(string[] args)
         {
+
         }
     }
 }
